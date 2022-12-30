c21995b2db34917174d1c21e0a50b2e264fc8d0a
AndersonElit <rodriarri@gmail.com>
2022-12-29 21:15:52 -0500

    version 1 linea base inclusion swagger (pendiente revisar error) 29-12-2022

--- infrastructure/entry-points/src/main/java/com/cleanarchitecture/ClearArchitectureBaseApp.java
+++ infrastructure/entry-points/src/main/java/com/cleanarchitecture/ClearArchitectureBaseApp.java
@@ -1,9 +1,11 @@
 package com.cleanarchitecture;
 
+import io.swagger.v3.oas.models.annotations.OpenAPI30;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPI30
 public class ClearArchitectureBaseApp {
 
     public static void main(String[] args) {
